fdb158a1e1243e0cff9e72964c7951baeac8f07a
Vignesh Murugaveeran <vc345companywrk@gmail.com>
2025-11-18 22:36:20 +0530

    Delete lib/consts directory

--- lib/consts/consts.dart
+++
@@ -1,8 +0,0 @@
-export './colors.dart';
-//export './colors.dart';
-export './images.dart';
-export './strings.dart';
-export './styles.dart';
-//from add dependency velocity_x: ^4.3.1
-export 'package:velocity_x/velocity_x.dart';
-export 'package:flutter/material.dart';
