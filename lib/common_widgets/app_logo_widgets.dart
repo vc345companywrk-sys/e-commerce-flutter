6d38b3b6a5152e30ea29d37b460d0bc6940c55ef
Vignesh Murugaveeran <vc345companywrk@gmail.com>
2025-11-18 22:36:41 +0530

    Delete lib/common_widgets directory

--- lib/common_widgets/app_logo_widgets.dart
+++
@@ -1,9 +0,0 @@
-import 'package:ecommerce_app/consts/consts.dart';
-
-Widget appLogoWidgets(double width, double height) {
-  return Image.asset(icAppLogo).box.white
-      .size(width, height)
-      .padding(EdgeInsetsGeometry.all(8))
-      .rounded
-      .make();
-}
